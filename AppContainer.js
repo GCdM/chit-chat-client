17af5ea1b6550a8a55c2da2ffb02036877649c84
GCdM <59828466+GCdM@users.noreply.github.com>
2020-01-22 09:33:50 +0000

    Set up app stackNavigator skeleton w/ 3 screens

---
+++ AppContainer.js
@@ -0,0 +1,25 @@
+import { createAppContainer } from 'react-navigation'
+import { createStackNavigator } from 'react-navigation-stack'
+
+import MainScreen from './screens/MainScreen'
+import ConversationScreen from './screens/ConversationScreen'
+import ProfileScreen from './screens/ProfileScreen'
+
+const AppNavigator = createStackNavigator(
+  {
+    Main: {
+      screen: MainScreen,
+    },
+    Conversation: {
+      screen: ConversationScreen,
+    },
+    Profile: {
+      screen: ProfileScreen,
+    },
+  },
+  {
+    initialRouteName: 'Main',
+  }
+)
+
+export default createAppContainer(AppNavigator)
---
+++ screens/ConversationScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default class ConversationScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Conversation Screen</Text>
+      </View>
+    );
+  }
+}
