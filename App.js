af83ed6bb0a9a44ae4bf14d19f00bc52afbb20ab
GCdM <59828466+GCdM@users.noreply.github.com>
2020-01-27 12:09:36 +0000

    Added redux store to app

--- App.js
+++ App.js
@@ -1,18 +1,28 @@
 import React from 'react'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
 
 import AppContainer from './src/containers/AppContainer'
 import AuthContainer from './src/containers/AuthContainer'
+import reducer from './src/reducer'
+
+const store = createStore(reducer)
 
 export default class App extends React.Component {
 
-  state = {
-    user: null,
+  get user() {
+    return store.getState().user
   }
-  
+
   render() {
-    // Placeholder until redux is bootstrapped
-    return this.state.user
-    ? <AppContainer />
-    : <AuthContainer />
+    return <Provider store={store}>
+      {
+        this.user
+        ? <AppContainer />
+        : <AuthContainer />
+      }
+    </Provider>
+    
   }
 }
+
